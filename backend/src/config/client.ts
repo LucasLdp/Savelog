c40bcc1c71e57bd58c27b02f83e7fc19409dd37b
LucasLdp <lucasdepauloldp1@gmail.com>
2025-11-02 11:41:15 -0300

    feat: add database client configuration with drizzle ORM

---
+++ backend/src/config/client.ts
@@ -0,0 +1,6 @@
+import * as schemas from "@src/database";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+export const db = drizzle(process.env.DATABASE_URL!, {
+  schema: { ...schemas },
+});
