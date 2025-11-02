1f2c65ae95fe58623a3b4121e2af9499b2d58b24
LucasLdp <lucasdepauloldp1@gmail.com>
2025-11-02 11:42:13 -0300

    feat: implement find user functionality with error handling

---
+++ backend/src/modules/users/cases/find-user.ts
@@ -0,0 +1,19 @@
+import { db } from "@src/config/client";
+import { NotFoundException } from "http-essentials";
+
+export async function findUser(id: string) {
+  const user = await db.query.user.findFirst({
+    where: (users, { eq }) => eq(users.id, id),
+    columns: {
+      password: false,
+    },
+  });
+
+  console.log(user);
+
+  if (!user) {
+    throw new NotFoundException("Usuário não encontrado");
+  }
+
+  return user;
+}
